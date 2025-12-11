22f95f945945be48de5e10e17a0e065b7d888660
Windintosh <48243825+Windintosh@users.noreply.github.com>
2025-12-11 12:12:50 +0900

    set cabinet components
    collision, staticmesh, materials coded in c++, which can be changed later

--- Source/EscapeTheOvertime/MainClass/Objects/Doors/CabinetDoor/CabinetDoor.cpp
+++ Source/EscapeTheOvertime/MainClass/Objects/Doors/CabinetDoor/CabinetDoor.cpp
@@ -1,13 +1,43 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+#include "CabinetDoor.h"
+#include "UObject/ConstructorHelpers.h"
 
-
-#include "MainClass/Objects/CabinetDoor/CabinetDoor.h"
 
 // Sets default values
 ACabinetDoor::ACabinetDoor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Component"));
+	SetRootComponent(BoxComp); // RootComponent = BoxComp;
+	BoxComp->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+	BoxComp->SetBoxExtent(FVector(50.f, 70.f, 15.f));
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
+	MeshComp->SetupAttachment(BoxComp);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> CabinetMesh(TEXT("/Game/EscapeTheOvertime/00_Maps/UseAsset/File_Cabinets_02-Freepoly_org/MiniCabinets_Door1.MiniCabinets_Door1'"));
+	if (CabinetMesh.Succeeded())
+	{
+		MeshComp->SetStaticMesh(CabinetMesh.Object);
+	} 
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to get StaticMesh for %s"), *GetName());
+	}
+	
+	static ConstructorHelpers::FObjectFinder<UMaterial> CabinetMaterial(TEXT("/Game/EscapeTheOvertime/00_Maps/UseAsset/File_Cabinets_02-Freepoly_org/21___Default.21___Default'"));
+	if (CabinetMaterial.Succeeded())
+	{
+		MeshComp->GetStaticMesh()->SetMaterial(0, CabinetMaterial.Object);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to get Material for %s"), *GetName());
+	}
+
+
+
 
 }
 
@@ -16,6 +46,12 @@ void ACabinetDoor::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	BoxComp->OnComponentBeginOverlap.AddDynamic(this, &ACabinetDoor::OnOverlapBegin);
+}
+
+void ACabinetDoor::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//add this later, after making character
 }
 
 // Called every frame
--- Source/EscapeTheOvertime/MainClass/Objects/Doors/CabinetDoor/CabinetDoor.h
+++ Source/EscapeTheOvertime/MainClass/Objects/Doors/CabinetDoor/CabinetDoor.h
@@ -1,9 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
 #include "CabinetDoor.generated.h"
 
 UCLASS()
@@ -15,9 +16,19 @@ public:
 	// Sets default values for this actor's properties
 	ACabinetDoor();
 
-protected:
+protected:	
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UBoxComponent* BoxComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* MeshComp;
+
+	UFUNCTION()
+	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
 
 public:	
 	// Called every frame
