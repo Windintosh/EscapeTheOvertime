4645cb1fa455a7a38b02969cdf72a92ddd8085a8
Windintosh <48243825+Windintosh@users.noreply.github.com>
2025-12-12 09:22:50 +0900

    Update RestroomDoor.cpp
    Now opens opposite direction when it is flipped

--- Source/EscapeTheOvertime/MainClass/Objects/Doors/RestroomDoors/RestroomDoor.cpp
+++ Source/EscapeTheOvertime/MainClass/Objects/Doors/RestroomDoors/RestroomDoor.cpp
@@ -10,6 +10,12 @@ void ARestroomDoor::BeginPlay()
 {
 	//
 	Super::BeginPlay();
+	if ((-270.f <= GetActorRotation().Yaw && GetActorRotation().Yaw <= -90.f) || (90.f <= GetActorRotation().Yaw && GetActorRotation().Yaw <= 270.f))
+	{
+		TargetYaw = InitialYaw + 90.f;
+	}
 
-	TargetYaw = InitialYaw - 90.f;
+	else {
+		TargetYaw = InitialYaw - 90.f;
+	}
 }
