e8604bae10473ea82a96cb918f4bd65eead80737
Windintosh <48243825+Windintosh@users.noreply.github.com>
2025-12-17 16:53:33 +0900

    Thrown item; need animation montage and logics to get it work

---
+++ Source/EscapeTheOvertime/MainClass/Objects/Items/Distraction/ThrownItem.cpp
@@ -0,0 +1,30 @@
+﻿#include "MainClass/Objects/Items/Distraction/ThrownItem.h"
+#include "Boss/Boss.h"
+#include "HorrorCharacter.h"
+#include "Kismet/GameplayStatics.h"
+
+AThrownItem::AThrownItem()
+{
+	ItemType = TEXT("ThrownItem");
+	ItemIndex = 999;
+}
+
+void AThrownItem::OnItemOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	ACharacter* PlayerCharacter = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+	AHorrorCharacter* GameCharacter = Cast<AHorrorCharacter>(PlayerCharacter);
+
+
+	GameCharacter->MakeNoise(5.f, GetActorLocation()); //Make noise at overlapped point, add condition(if boss) if glitches
+
+	ActivateItem(OtherActor); //do sth to boss
+}
+
+void AThrownItem::ActivateItem(AActor* Activator)
+{
+	ABoss* Boss = Cast<ABoss>(Activator);
+	if (!Boss) return;
+
+	//Make boss stunned or sth
+
+}
---
+++ Source/EscapeTheOvertime/MainClass/Objects/Items/Distraction/ThrownItem.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "MainClass/Objects/Items/ItemBase.h"
+#include "ThrownItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPETHEOVERTIME_API AThrownItem : public AItemBase
+{
+	GENERATED_BODY()
+	
+protected:
+
+	AThrownItem();
+
+	virtual void OnItemOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+
+	virtual void ActivateItem(AActor* Activator) override;
+
+};
