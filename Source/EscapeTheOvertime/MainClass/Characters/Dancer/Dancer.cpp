53ab88e1adad3513ca2e4950905a1ab2f4f3c3ae
Windintosh <48243825+Windintosh@users.noreply.github.com>
2025-12-24 12:45:18 +0900

    EQS System w/ object blueprints; might need some minor changes
    currently in BT_BossV2, copy&paste the logic if needed

--- Source/EscapeTheOvertime/MainClass/Characters/Dancer/Dancer.cpp
+++ Source/EscapeTheOvertime/MainClass/Characters/Dancer/Dancer.cpp
@@ -11,15 +11,12 @@ ADancer::ADancer()
     // 1. NavModifier 컴포넌트 생성
     NavModifierComp = CreateDefaultSubobject<UNavModifierComponent>(TEXT("NavModifierComp"));
 
-    // 2. 컴포넌트를 루트(캡슐)에 부착하여 캐릭터를 따라다니게 설정
-    //NavModifierComp->SetupAttachment(RootComponent);
-
-    // 3. 영역 클래스(Area Class) 설정
+    // 2. 영역 클래스(Area Class) 설정
     // UNavArea_Obstacle은 기본적으로 높은 비용을 가집니다.
     // 다른 AI는 이 영역을 피해 경로를 잡으려 노력합니다.
     NavModifierComp->SetAreaClass(UNavArea_Obstacle::StaticClass());
 
-    // 4. 범위 설정 (선택 사항)
+    // 3. 범위 설정 (선택 사항)
     // 캐릭터의 캡슐 크기에 맞춰 범위를 조정합니다.
     // FVector(X, Y, Z) 크기입니다. 적절한 크기로 조절하세요.
     NavModifierExtentSize = FVector(50.f, 50.f, 100.f);
