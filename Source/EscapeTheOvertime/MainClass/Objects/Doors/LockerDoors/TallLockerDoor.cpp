379a0ec2eb0a9af390d7965ad204020db289ff39
Windintosh <48243825+Windintosh@users.noreply.github.com>
2025-12-31 15:49:25 +0900

    removed potentially faulty codes

--- Source/EscapeTheOvertime/MainClass/Objects/Doors/LockerDoors/TallLockerDoor.cpp
+++ Source/EscapeTheOvertime/MainClass/Objects/Doors/LockerDoors/TallLockerDoor.cpp
@@ -5,15 +5,15 @@ ATallLockerDoor::ATallLockerDoor()
 	BoxComp->SetBoxExtent(FVector(2.f, 21.f, 117.f));
 	BoxComp->SetRelativeLocation(FVector(0.f, 0.f, 117.f), false);
 	MeshComp->SetRelativeLocation(FVector(0.f, -20.5f, -116.f), false);
-	static ConstructorHelpers::FObjectFinder<UStaticMesh> TallLockerMesh(TEXT("/Game/EscapeTheOvertime/00_Maps/UseAsset/Cabinet/OfficeCabinetDoor4.OfficeCabinetDoor4"));
-	if (TallLockerMesh.Succeeded())
-	{
-		MeshComp->SetStaticMesh(TallLockerMesh.Object);
-	}
-	else
-	{
-		UE_LOG(LogTemp, Error, TEXT("Failed to get StaticMesh for %s"), *GetName());
-	}
+	//static ConstructorHelpers::FObjectFinder<UStaticMesh> TallLockerMesh(TEXT("/Game/EscapeTheOvertime/00_Maps/UseAsset/Cabinet/OfficeCabinetDoor4.OfficeCabinetDoor4"));
+	//if (TallLockerMesh.Succeeded())
+	//{
+	//	MeshComp->SetStaticMesh(TallLockerMesh.Object);
+	//}
+	//else
+	//{
+	//	UE_LOG(LogTemp, Error, TEXT("Failed to get StaticMesh for %s"), *GetName());
+	//}
 
 	//static ConstructorHelpers::FObjectFinder<UMaterial> TallLockerMaterial(TEXT("/Game/EscapeTheOvertime/00_Maps/UseAsset/Cabinet/Material_001.Material_001"));
 	//if (TallLockerMaterial.Succeeded())
